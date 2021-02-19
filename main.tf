598d82f3092618b4f012d247c06cb795a990f3dd
Moritz Freyburger <moritz@freyburger.io>
2021-02-19 18:38:54 +0100

    Update version to 2.48.0

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
     required_providers {
         azurerm = {
-            version = "2.47.0"
+            version = "2.48.0"
         }
         azuread = {
             version = "1.3.0"
